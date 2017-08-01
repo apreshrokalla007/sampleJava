7991aac9d49d3fc7504817c23177c34e8ab712b1
arokalla <apresh.roakalla@appshark.com>
2017-08-01 13:40:44 +0530

    Changes

--- SampleJava/src/SampleJava.java
+++ SampleJava/src/SampleJava.java
@@ -12,8 +12,7 @@ public class SampleJava {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Test Method");//Added Comment
-		System.out.println("Test Method2");
+		System.out.println("Test Method");
 		System.out.println("Test Method3");
 	}
 

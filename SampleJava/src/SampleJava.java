c44115054ca1e9efbccae8bbd3ee961a9c90a301
arokalla <apresh.roakalla@appshark.com>
2017-09-13 11:51:20 +0530

    changed

--- SampleJava/src/SampleJava.java
+++ SampleJava/src/SampleJava.java
@@ -14,8 +14,8 @@ public class SampleJava {
 	public static void main(String[] args) {
 		System.out.println("Test Method3210023");//Added Comment
 		System.out.println("Test Method234111111111");
-		System.out.println("Test Method234340023");
 		System.out.println("Commit from Second client");
+		System.out.println("Test Method234340023");
 		System.out.println("Test Method234");
 		System.out.println("Test Method3");
 	}

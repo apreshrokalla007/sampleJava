79a0c5eee5d3d2e07bd06e0fc2aa896635e1032e
arokalla <apresh.roakalla@appshark.com>
2017-09-13 12:05:01 +0530

    modified

--- SampleJava/src/SampleJava.java
+++ SampleJava/src/SampleJava.java
@@ -12,13 +12,8 @@ public class SampleJava {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Test Method3210023");//Added Comment
-		System.out.println("Test Method234111111111");
-		System.out.println("Commit from Second client");
-		System.out.println("Test Method234340023");
-		System.out.println("Test Method234");
-		System.out.println("Test Method3");
-		System.out.println("Test Method4");
+		System.out.println("Test Method1");
+		System.out.println("Test Method2");
 	}
 
 }

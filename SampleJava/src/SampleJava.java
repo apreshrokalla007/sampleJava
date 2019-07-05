431698a718c5781b69b4b98e857909903ba6c9cd
aappshark <apresh.rokalla@appshark.com>
2019-07-05 19:58:04 +0530

    * New Branch 1

--- SampleJava/src/SampleJava.java
+++ SampleJava/src/SampleJava.java
@@ -13,7 +13,7 @@ public class SampleJava {
 	 */
 	public static void main(String[] args) {
 
-		System.out.println("Commit 2 by aappshark");
+		System.out.println("Commit 1 on branch by aappshark");
 
 		System.out.println("Test Method4");System.out.println("Test Method4");System.out.println("Test Method4");
 	}

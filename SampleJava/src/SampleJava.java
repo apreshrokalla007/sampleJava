5c37c8dea287ecd726ef9ca4cbb4d22988d95f83
aappshark <apresh.rokalla@appshark.com>
2019-07-05 20:03:14 +0530

    * Added Test File

--- SampleJava/src/SampleJava.java
+++ SampleJava/src/SampleJava.java
@@ -14,8 +14,14 @@ public class SampleJava {
 	public static void main(String[] args) {
 
 		System.out.println("Commit 1 on branch by aappshark");
+		
+		System.out.println("Added New Line 1 by aappshark");
+		
+		System.out.println("Added New Line 2 by aappshark");
 
 		System.out.println("Test Method4");System.out.println("Test Method4");System.out.println("Test Method4");
+		
+		System.out.println("Added New Line 3 by aappshark");
 	}
 
 }

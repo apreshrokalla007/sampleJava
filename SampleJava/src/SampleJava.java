88756e2c8e62a1fe3cb1d6125cd46c6d13d7098c
agmail <apreshrokalla@gmail.com>
2019-07-05 20:07:19 +0530

    * Local Commit 2 in master by agmail

--- SampleJava/src/SampleJava.java
+++ SampleJava/src/SampleJava.java
@@ -19,12 +19,17 @@ public class SampleJava {
 		
 		System.out.println("Added New Line 2 by aappshark");
 
+		System.out.println("Commit 1 by agmail");
 		System.out.println("Test Method4");System.out.println("Test Method4");System.out.println("Test Method4");
 		
 		System.out.println("Added New Line 3 by aappshark");
 		
 		System.out.println("Commit 2 on branch by aappshark\");"
 				System.out.println("Commit 2 on branch by aappshark\");
+		
+		System.out.println("Commit 2 by agmail");
+		
+		System.out.println("Commit 2 by agmail");
 	}
 
 }
